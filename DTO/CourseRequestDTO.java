c7866c8afa6a04c0eefb08c169b770df111a0be9
MinThwayKhaing <110376802+MinThwayKhaing@users.noreply.github.com>
2022-08-14 12:52:22 +0630

    Add files via upload

---
+++ DTO/CourseRequestDTO.java
@@ -0,0 +1,20 @@
+package DTO;
+
+public class CourseRequestDTO 
+{
+	private String courseid;
+	private String coursename;
+	public String getCourseid() {
+		return courseid;
+	}
+	public void setCourseid(String courseid) {
+		this.courseid = courseid;
+	}
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	
+}
---
+++ DTO/UserResponseDTO.java
@@ -0,0 +1,43 @@
+package DTO;
+
+public class UserResponseDTO 
+{
+	private String username;
+	private String userid;
+	private String email;
+	private String password;
+	private String userrole;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserrole() {
+		return userrole;
+	}
+	public void setUserrole(String userrole) {
+		this.userrole = userrole;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+}
