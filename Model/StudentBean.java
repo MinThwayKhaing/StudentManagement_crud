18c314b4aea0d33f9746a9bd50b1d3e7adce75ea
MinThwayKhaing <110376802+MinThwayKhaing@users.noreply.github.com>
2022-08-14 12:55:01 +0630

    Add files via upload

---
+++ Model/StudentBean.java
@@ -0,0 +1,60 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class StudentBean 
+{
+	private String studentid;
+	private String studentname;
+	private String dob;
+	private String gender;
+	private String phone;
+	private String education;
+	private String  course;
+ 
+	public String getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(String studentid) {
+		this.studentid = studentid;
+	}
+	public String getStudentname() {
+		return studentname;
+	}
+	public void setStudentname(String studentname) {
+		this.studentname = studentname;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+		
+	
+	
+}
---
+++ Model/UserSearchBean.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class UserSearchBean 
+{
+	private String searchid;
+	private String searchname;
+	public String getSearchid() {
+		return searchid;
+	}
+	public void setSearchid(String searchid) {
+		this.searchid = searchid;
+	}
+	public String getSearchname() {
+		return searchname;
+	}
+	public void setSearchname(String searchname) {
+		this.searchname = searchname;
+	}
+	
+	
+	
+}
